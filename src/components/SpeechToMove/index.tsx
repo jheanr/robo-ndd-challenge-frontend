17ed776a72ba342ee87aa30890ca3334c7332905
Jhean Ramos <jheanr@gmail.com>
2021-08-20 16:54:18 -0300

    fix: speech recognition

--- src/components/SpeechToMove/index.tsx
+++ src/components/SpeechToMove/index.tsx
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import { FiMic } from 'react-icons/fi';
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
 import SpeechRecognition, {
